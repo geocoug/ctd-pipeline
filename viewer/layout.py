9f2e7242c70b6e281cb1edeb44f12e7edf01e8c9
Caleb Grant <geocoug@gmail.com>
2023-04-28 11:48:26 -0700

    add link to integral site from logo
    Signed-off-by: Caleb Grant <geocoug@gmail.com>

--- viewer/layout.py
+++ viewer/layout.py
@@ -15,24 +15,35 @@ def grid() -> list:
                             [
                                 dbc.Col(
                                     html.H3(
-                                        "ASV CTD Data Viewer",
+                                        html.A(
+                                            "ASV CTD Data Viewer",
+                                            href="#",
+                                            style={
+                                                "text-decoration": "inherit",
+                                                "color": "inherit",
+                                            },
+                                        ),
                                         className="p-2 mx-auto my-auto",
                                         style={"color": "#c8d3d5"},
                                     ),
                                     className="col justify-content-center text-center",
                                 ),
                                 dbc.Col(
-                                    html.Img(
-                                        src="assets/integral.png",
-                                        className="mx-auto",
-                                        style={
-                                            "width": "75px",
-                                            "height": "50px",
-                                        },
+                                    html.A(
+                                        html.Img(
+                                            src="assets/integral.png",
+                                            className="mx-auto",
+                                            style={
+                                                "width": "75px",
+                                                "height": "50px",
+                                            },
+                                        ),
+                                        href="https://integral-corp.com",
                                     ),
                                     className="col-md-3 col-4 mx-auto my-auto",
                                 ),
                             ],
+                            className="p-2",
                             style={"background-color": "#2c353a"},
                         ),
                         dbc.Row(
